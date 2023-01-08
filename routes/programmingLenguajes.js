697409dd024400772ddf3452ead7a304de0566a9
gladys5 <yumanji@live.com>
2023-01-08 04:14:06 -0700

    peticiones get post y path desde postman

--- routes/programmingLenguajes.js
+++ routes/programmingLenguajes.js
@@ -4,7 +4,25 @@ const programminLenguaje = require("../services/programmingLenguaje")
 
 router.get("/", async function (req, res, next) {
   try {
-    res.json(await programminLenguaje.getmultiple(req.query.page))
+    res.json(await programminLenguaje.read(req.query.page))
+  } catch (err) {
+    console.error(err.message)
+    next(err)
+  }
+})
+
+router.post("/", async function (req, res, next) {
+  try {
+    res.json(await programminLenguaje.create(req.body))
+  } catch (err) {
+    console.error(err.message)
+    next(err)
+  }
+})
+
+router.patch("/", async function (req, res, next) {
+  try {
+    res.json(await programminLenguaje.update(req.body.id, req.body))
   } catch (err) {
     console.error(err.message)
     next(err)
--- services/programmingLenguaje.js
+++ services/programmingLenguaje.js
@@ -1,10 +1,42 @@
 const db = require("./db")
 const helper = require("../helper")
 const config = require("../config")
-async function getmultiple(page = 1) {
+
+async function create(programminLanguaje) {
+  const resultado = await db.query(`INSERT INTO lenguajes_programacion
+    (nombre,anio_salida,github_rank)
+    VALUES
+    (
+        '${programminLanguaje.nombre}',
+        '${programminLanguaje.anio_salida}',
+       ' ${programminLanguaje.github_rank}'
+    )
+    `)
+  let message = "Error al actualizar el lenguaje de programacion"
+  if (resultado.affectedRows) {
+    message = "la peticion se realizo con exito"
+  }
+  return { message }
+}
+async function update(id, programminLanguaje) {
+  const resultado = await db.query(`
+    UPDATE lenguajes_programacion
+    SET nombre= '${programminLanguaje.nombre}',
+    anio_salida= '${programminLanguaje.anio_salida}',
+    github_rank=' ${programminLanguaje.github_rank}' WHERE id ='${id}'
+    `)
+  let message = "Error al actualizar el lenguaje de programacion"
+  if (resultado.affectedRows) {
+    message = "la peticion se realizo con exito"
+  }
+  return { message }
+}
+
+async function read(page = 1) {
   const offset = helper.getOffSet(page, config.listPerPage)
   const rows = await db.query(` 
         SELECT *FROM lenguajes_programacion LIMIT ${offset},${config.listPerPage}`)
+  // SELECT *FROM lenguajes_programacion LIMIT ${offset},${config.listPerPage}`
   const data = helper.emptyRows(rows)
   const metadata = { page }
   return {
@@ -12,6 +44,9 @@ async function getmultiple(page = 1) {
     metadata,
   }
 }
+
 module.exports = {
-  getmultiple,
+  read,
+  create,
+  update,
 }
